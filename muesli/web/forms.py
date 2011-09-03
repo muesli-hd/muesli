aabc7b15dccf8bddde5343b28c71c5ef3dc3bb26
Matthias Kümmerer <matthias@matthias-k.org>
2011-09-03 23:34:46 +0200

    Bugfix
    Signed-off-by: Matthias Kümmerer <matthias@matthias-k.org>

--- muesli/web/forms.py
+++ muesli/web/forms.py
@@ -6,19 +6,19 @@ class Form(object):
 		self.schema = schema
 		self.value = {}
 		if obj is not None:
-		for f in fields:
-			self.value[f] = getattr(obj, f)
+			for f in fields:
+				self.value[f] = getattr(obj, f)
 	def validate(self, value):
 		self.errors = {}
 		try:
-		self.value.update(self.schema.to_python(value))
-		return True
+			self.value.update(self.schema.to_python(value))
+			return True
 		except formencode.Invalid as exc:
-		self.errors = exc.unpack_errors()
+			self.errors = exc.unpack_errors()
 		#for key, e in exc.error_dict.iteritems():
 		#  self.errors[key] = e.msg
 		#self.errors = exc.error_dict
-		return False
+			return False
 	__getitem__ = lambda self, key: self.value.get(key, "")
 	__contains__ = lambda self, key: key in self.value
 	__iter__ = lambda self: self.value.iterkeys()
