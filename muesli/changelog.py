fad6567d91d249d281f07b69a17440c5fac6e5ba
Matthias Kümmerer <matthias@matthias-k.org>
2012-03-02 23:15:21 +0100

    Changelog
    Signed-off-by: Matthias Kümmerer <matthias@matthias-k.org>

---
+++ muesli/changelog.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+#
+# utils.py
+#
+# This file is part of MUESLI.
+#
+# Copyright (C) 2012, Matthias Kuemmerer <matthias (at) matthias-k.org>
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+changelog=u"""
+====2012-03-02
+concerns:assistants
+Es ist jetzt möglich, für jede Vorlesung einzustellen, ob die Tutoren Punkte für alle
+Übungsgruppen oder nur für ihre eigenen Übungsgruppen eintragen dürfen, oder auch, dass
+sie gar keine Punkte eintragen dürfen. Die Option dazu findet sich auf der "Bearbeiten"-Seite
+der Vorlesung.
+
+====2012-02-02
+concerns:assistants
+Die Punkte von Testaten können versteckt werden. Dies bewirkt, dass die Studenten ihre
+Punktzahlen in diesem Testat nicht sehen. Die Option findet sich auf der "Bearbeiten"-Seite
+des Testats.
+"""
