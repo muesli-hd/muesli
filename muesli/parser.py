69dc6c43fab5313c2ab977c1eeded20beb6fc65c
Matthias Kümmerer <matthias@matthias-k.org>
2012-03-16 14:17:15 +0100

    Bugfix: Parser hat None teilweise falsch behandelt
    Signed-off-by: Matthias Kümmerer <matthias@matthias-k.org>

--- muesli/parser.py
+++ muesli/parser.py
@@ -92,9 +92,9 @@ class Parser(object):
 		# map operator symbols to corresponding arithmetic operations
 		self.opn = { "+" : self.handleNone( lambda a,b: a + b ),
 				"-" : self.handleNone( lambda a,b: a - b ),
-				"*" : self.handleNone( lambda a,b: a * b ),
-				"/" : self.handleNone( lambda a,b: a / b ),
-				"^" : self.handleNone( lambda a,b: a ** b ) }
+				"*" : self.handleNone( lambda a,b: a * b, none_survives=True ),
+				"/" : self.handleNone( lambda a,b: a / b, none_survives=True ),
+				"^" : self.handleNone( lambda a,b: a ** b, none_survives=True ) }
 		self.functions = { 'max': max,
 			'min': self.min,
 			'cases': self.cases,
@@ -105,12 +105,17 @@ class Parser(object):
 	def min(self, arr):
 		arr = filter(lambda a: a != None, arr)
 		return min(arr)
-	def handleNone(self, func):
+	def handleNone(self, func, none_survives=False):
 		def newFunc(a, b):
 			if a == None:
-				return b
+				if none_survives:
+					return None
+				else:
+					return b
 			if b == None:
-				return a
+				if none_survives:
+					return None
+				else: return a
 			else:
 				return func(a,b)
 		return newFunc
