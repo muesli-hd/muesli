88e360d437c8ca154df9746d58dcdefd39e14f1c
Matthias Kümmerer <matthias@matthias-k.org>
2012-04-13 12:00:01 +0200

    Mehrere Assistenten pro Vorlesung in Datenbank angelegt
    Signed-off-by: Matthias Kümmerer <matthias@matthias-k.org>

---
+++ alembic/versions/14d5e28d3626_lecture_assistants.py
@@ -0,0 +1,42 @@
+"""lecture_assistants
+
+Revision ID: 14d5e28d3626
+Revises: 2a1638d875ac
+Create Date: 2012-04-13 11:50:50.501793
+
+"""
+
+# revision identifiers, used by Alembic.
+revision = '14d5e28d3626'
+down_revision = '2a1638d875ac'
+
+from alembic import op
+import sqlalchemy as sa
+from sqlalchemy.dialects import postgresql
+
+def upgrade():
+	### Caution: Only works in online mode!!!
+	op.create_table('lecture_assistants',
+		sa.Column('lecture', sa.Integer(), nullable=False),
+		sa.Column('assistant', sa.Integer(), nullable=False),
+		sa.ForeignKeyConstraint(['assistant'], ['users.id'], ),
+		sa.ForeignKeyConstraint(['lecture'], ['lectures.id'], ),
+		sa.PrimaryKeyConstraint('lecture', 'assistant')
+	)
+	lectures = sa.sql.table('lectures', 
+		sa.Column('id', sa.Integer),
+		sa.Column('assistant', sa.Integer)
+	)
+	connection = op.get_bind()
+	results = connection.execute(lectures.select())
+	ins = []
+	for res in results:
+		ins.append({'lecture': res.id, 'assistant': res.assistant})
+	lecture_assistants = sa.sql.table('lecture_assistants',
+		sa.Column('lecture', sa.Integer(), nullable=False),
+		sa.Column('assistant', sa.Integer(), nullable=False)
+	)
+	op.bulk_insert(lecture_assistants,ins)
+
+def downgrade():
+    op.drop_table('lecture_assistants')
