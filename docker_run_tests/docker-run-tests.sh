edb6edbdeaae4b1819addd31365d2721bc9e010d
Christian Heusel <christian@heusel.eu>
2019-01-02 13:58:49 +0100

    for every test the db is now upgraded
    fixes #78
    Signed-off-by: Christian Heusel <christian@heusel.eu>

--- docker_run_tests/docker-run-tests.sh
+++ docker_run_tests/docker-run-tests.sh
@@ -2,5 +2,8 @@
 sed 's/\/\/\//\/\/postgres@postgres\//' muesli.yml.example | sed 's/localhost/0.0.0.0/' > muesli.yml
 sed 's/\/\/\//\/\/postgres@postgres\//' alembic.ini.example > alembic.ini
 sleep 5;
+sed -i '/^sqlalchemy.url = postgres:\/\/postgres@postgres\/muesli/ d' alembic.ini
+sed -i 's/^\#sqlalchemy.url = postgres:\/\/postgres@postgres\/muesli/sqlalchemy.url = postgres:\/\/postgres@postgres\/muesli/' alembic.ini
+alembic upgrade head
 python3 -m smtpd -n -c DebuggingServer localhost:25 &
 MUESLI_PATH=$(pwd) py.test --cov=muesli muesli/tests && exit
