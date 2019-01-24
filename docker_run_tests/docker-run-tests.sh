d1ea833db755de69001248926b0599e0088f23d5
Christian Heusel <christian@heusel.eu>
2019-01-24 01:01:07 +0100

    3s seems to be enough
    Signed-off-by: Christian Heusel <christian@heusel.eu>

--- docker_run_tests/docker-run-tests.sh
+++ docker_run_tests/docker-run-tests.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 sed 's/\/\/\//\/\/postgres@postgres\//' muesli.yml.example | sed 's/localhost/0.0.0.0/' > muesli.yml
 sed 's/\/\/\//\/\/postgres@postgres\//' alembic.ini.example > alembic.ini
-sleep 5;
+sleep 3;
 sed -i '/^sqlalchemy.url = postgres:\/\/postgres@postgres\/muesli/ d' alembic.ini
 sed -i 's/^\#sqlalchemy.url = postgres:\/\/postgres@postgres\/muesli/sqlalchemy.url = postgres:\/\/postgres@postgres\/muesli/' alembic.ini
 alembic upgrade head
