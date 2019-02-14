d00f9e405f51da61abee28da04af35daac6115d8
mynameisinuse <>
2019-02-14 12:45:41 +0000

    Neue Tabelle zum Speichern der Emailpräferenzen. Und Anpassung von docker-serve, damit alembic funktioniert.

--- muesli/models.py
+++ muesli/models.py
@@ -586,3 +586,9 @@ class StudentGrade(Base):
     student_id = Column('student', Integer, ForeignKey(User.id), nullable=False, primary_key=True)
     student = relationship(User, backref=backref('student_grades', lazy='dynamic'))
     grade = Column(Numeric(precision=2, scale=1), CheckConstraint('grade >= 1.0 AND grade <= 5.0'))
+
+class EmailPreferences(Base):
+    __tablename__ = 'email_preferences'
+    user_id = Column(Integer, ForeignKey(User.id), nullable=False, primary_key=True)
+    lecture_id = Column(Integer, ForeignKey(Lecture.id), nullable=False, primary_key=True)
+    receive_status_mails = Column(Boolean, nullable=False)
