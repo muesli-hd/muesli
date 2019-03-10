119fa6e3a0a51135ff1eba33982360dcacbaf8e9
Dennis <dennis.pfleger@web.de>
2019-03-10 22:54:44 +0100

    Google Style Guidelines

--- muesli/tests/seleniumTests.py
+++ muesli/tests/seleniumTests.py
@@ -1,118 +1,98 @@
-from selenium import webdriver
+"""SeleniumTests for export stuff, tooltips and histograms"""
 import time
+from selenium import webdriver
 
-def initDriver():
+
+def initiate_driver(user, key):
+    """Initiate Chrome Webdriver with path to chromedriver and address localhost:8080
+    Returns:
+        Chrome Webdriver with a logged in (admin) user for the muesli page
+    """
     driver = webdriver.Chrome("SeleniumDriver/chromedriver")
+    driver.maximize_window()
     driver.get("localhost:8080")
+
+    login_button = driver.find_element_by_xpath("//a[@href='/user/login']")
+    login_button.click()
+
+    email = driver.find_element_by_xpath("//input[@name='email']")
+    password = driver.find_element_by_xpath("//input[@name='password'][@type='password']")
+
+    # Login with own Admin credentials
+    email.send_keys(user)
+    password.send_keys(key)
+    login = driver.find_element_by_xpath("//input[@type='submit']")
+    login.click()
 
     return driver
 
-def excelTest():
-    driver = initDriver()
 
-    loginBut = driver.find_element_by_xpath("//a[@href='/user/login']")
-    loginBut.click()
+def export_test():
+    """Check login and test all excel exports
+    Raises:
+        NoSuchElementException: Element could not be found.
+    """
+    driver = initiate_driver("dennis.pfleger@web.de", "a")
 
-    email = driver.find_element_by_xpath("//input[@name='email']")
-    pw = driver.find_element_by_xpath("//input[@name='password'][@type='password']")
-
-    email.send_keys("dennis.pfleger@web.de")
-    pw.send_keys("a")
-    login = driver.find_element_by_xpath("//input[@type='submit']").click()
-
-    #wait for page to load
+    # Wait for page to load
     time.sleep(2)
 
-    #Check admin tab
+    # Check and click admin tab
     administration = driver.find_element_by_xpath("//a[@href='/admin']")
     administration.click()
 
-    #Export all lectures and tutorials as excel file
-    excelLink = driver.find_element_by_xpath("//a[@href='/lecture/export_excel/downloadDetailTutorials.xlsx']")
-    excelLink.click()
+    # Check Yaml exports
+    lecture_yaml = driver.find_element_by_xpath("//a[@href='/lecture/export_yaml']")
+    lecture_yaml.click()
 
-    #Check one lecture and grading excel export
+    lecture_yaml_all = driver.find_element_by_xpath("//a[@href='/lecture/export_yaml"
+                                                    "_details?show_all=1']")
+    lecture_yaml_all.click()
+
+    lecture_tutorials_yaml = driver.find_element_by_xpath("//a[@href='/lecture/"
+                                                          "export_yaml_details']")
+    lecture_tutorials_yaml.click()
+
+    lecture_tutorials_yaml_all = driver.find_element_by_xpath("//a[@href='/lecture/"
+                                                              "export_yaml_details?show_all=1']")
+    lecture_tutorials_yaml_all.click()
+
+    # Export all lectures and tutorials as excel file
+    excel_link = driver.find_element_by_xpath("//a[@href='/lecture/export_excel/"
+                                             "downloadDetailTutorials.xlsx']")
+    excel_link.click()
+
+    # Check one lecture and grading excel export
     lectures = driver.find_element_by_xpath("//a[@href='/lecture/list']")
     lectures.click()
 
     lecture = driver.find_element_by_xpath("//a[@href='/lecture/view/20109']")
     lecture.click()
 
-    lectureEdit = driver.find_element_by_xpath("//a[@href='/lecture/edit/20109']")
-    lectureEdit.click()
-
-    #Check specific grading
-    gradings = driver.find_element_by_xpath("//a[@href='/grading/edit/6692']")
-    gradings.click()
-    grades = driver.find_element_by_xpath("//a[@href='/grading/enter_grades/6692']")
-    grades.click()
-
-    #excel export
-    excelExport = driver.find_element_by_xpath("//a[@href='/grading/export/Pruefung%206692.xlsx']")
-    excelExport.click()
-
-    time.sleep(3)
-
-def checkTooltips():
-    driver = initDriver()
-
-    loginBut = driver.find_element_by_xpath("//a[@href='/user/login']")
-    loginBut.click()
-
-    email = driver.find_element_by_xpath("//input[@name='email']")
-    pw = driver.find_element_by_xpath("//input[@name='password'][@type='password']")
-
-    email.send_keys("dennis.pfleger@web.de")
-    pw.send_keys("a")
-    ĺogin = driver.find_element_by_xpath("//input[@type='submit']").click()
-
-    # wait for page to load
-    time.sleep(2)
-
-    # Check lectures tab
-    lectures = driver.find_element_by_xpath("//a[@href='/lecture/list']")
-    lectures.click()
-
-    #Check lecture x
-    lecture = driver.find_element_by_xpath("//a[@href='/lecture/view/20109']")
-    lecture.click()
-
-    #Edit lecture x
     lecture_edit = driver.find_element_by_xpath("//a[@href='/lecture/edit/20109']")
     lecture_edit.click()
 
-    #Check if tooltip exists and check text
-    tooltipGrading = driver.find_element_by_xpath("//p[contains(text(), 'Hier können Noten anhand von einer oder "
-                                                  "mehreren Klausuren/Testaten berechnet und eingetragen werden.')]")
-    tooltipTutor = driver.find_element_by_xpath("//p[contains(text(), 'Klicken Sie auf den Namen eines Tutors, um ihm/ihr eine Email zu schicken.')]")
-
-    #Check excel tooltip
+    # Check specific grading
     gradings = driver.find_element_by_xpath("//a[@href='/grading/edit/6692']")
     gradings.click()
     grades = driver.find_element_by_xpath("//a[@href='/grading/enter_grades/6692']")
     grades.click()
 
-    excelTooltip = driver.find_element_by_xpath("//p[contains(text(), 'Hier können Sie die Klausurergebnisse "
-                                                "als Exceldatei (.xlsx) erstellen und herunterladen.')]")
-
+    # Excel export
+    excel_export = driver.find_element_by_xpath("//a[@href='/grading/export/Pruefung%206692.xlsx']")
+    excel_export.click()
 
     time.sleep(3)
 
 
-def histogramTest():
-    driver = initDriver()
+def check_tooltips():
+    """Check tooltips for their existence and their text
+    Raises:
+        NoSuchElementException: Element could not be found.
+    """
+    driver = initiate_driver("dennis.pfleger@web.de", "a")
 
-    loginBut = driver.find_element_by_xpath("//a[@href='/user/login']")
-    loginBut.click()
-
-    email = driver.find_element_by_xpath("//input[@name='email']")
-    pw = driver.find_element_by_xpath("//input[@name='password'][@type='password']")
-
-    email.send_keys("dennis.pfleger@web.de")
-    pw.send_keys("a")
-    ĺogin = driver.find_element_by_xpath("//input[@type='submit']").click()
-
-    # wait for page to load
+    # Wait for page to load
     time.sleep(2)
 
     # Check lectures tab
@@ -127,17 +107,62 @@ def histogramTest():
     lecture_edit = driver.find_element_by_xpath("//a[@href='/lecture/edit/20109']")
     lecture_edit.click()
 
-    #Check histogram
+    # Check if tooltip exists and check text, variables should remain unused
+    tooltip_grading = driver.find_element_by_xpath("//p[contains(text(), 'Hier können "
+                                                  "Noten anhand von einer oder mehreren Klausuren/"
+                                                  "Testaten berechnet und eingetragen werden.')]")
+
+    tooltip_tutor = driver.find_element_by_xpath("//p[contains(text(), "
+                                                 "'Klicken Sie auf den Namen eines "
+                                                "Tutors, um ihm/ihr eine Email zu schicken.')]")
+
+    # Check excel tooltip
+    gradings = driver.find_element_by_xpath("//a[@href='/grading/edit/6692']")
+    gradings.click()
+    grades = driver.find_element_by_xpath("//a[@href='/grading/enter_grades/6692']")
+    grades.click()
+
+    excel_tooltip = driver.find_element_by_xpath("//p[contains(text(), 'Hier können Sie die "
+                                                "Klausurergebnisse als Exceldatei (.xlsx) "
+                                                 "erstellen und herunterladen.')]")
+
+    time.sleep(3)
+
+
+def histogram_test():
+    """Check if histograms exist
+    Raises:
+        NoSuchElementException: Element could not be found.
+    """
+    driver = initiate_driver("dennis.pfleger@web.de", "a")
+
+    # Wait for page to load
+    time.sleep(2)
+
+    # Check lectures tab
+    lectures = driver.find_element_by_xpath("//a[@href='/lecture/list']")
+    lectures.click()
+
+    # Check lecture x
+    lecture = driver.find_element_by_xpath("//a[@href='/lecture/view/20109']")
+    lecture.click()
+
+    # Edit lecture x
+    lecture_edit = driver.find_element_by_xpath("//a[@href='/lecture/edit/20109']")
+    lecture_edit.click()
+
+    # Check histogram
     statistics = driver.find_element_by_xpath("//a[@href='/exam/statistics/13417/']")
     statistics.click()
 
-    #Wait for histogram to load and get it
+    # Wait for histogram to load and get it
     time.sleep(2)
     driver.find_element_by_xpath("//img[@src='/exam/histogram_for_exam/13417/']")
 
     time.sleep(3)
 
-if __name__== "__main__":
-    excelTest()
-    checkTooltips()
-    histogramTest()
+
+if __name__ == "__main__":
+    export_test()
+    check_tooltips()
+    histogram_test()
