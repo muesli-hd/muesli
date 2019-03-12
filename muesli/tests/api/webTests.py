8fdd0bdc8ed4d0030a0895e3eb3ea9d4ca5b91a1
Christian Heusel <christian@heusel.eu>
2019-03-12 05:39:14 +0100

    added some webtests
    Signed-off-by: Christian Heusel <christian@heusel.eu>

--- muesli/tests/api/webTests.py
+++ muesli/tests/api/webTests.py
@@ -20,9 +20,48 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
+import random
+import string
+
 from muesli.tests import functionalTests
+from muesli.models import BearerToken
 
 
 class BaseTests(functionalTests.BaseTests):
+    def test_api_keys(self):
+        self.testapp.get('/user/api_keys', status=403)
+
     def test_api_explorer(self):
         self.testapp.get('/api-explorer', status=200)
+
+class UserLoggedInTests(functionalTests.PopulatedTests):
+    def setUp(self):
+        functionalTests.PopulatedTests.setUp(self)
+        self.setUser(self.user)
+
+    def test_api_keys(self):
+        self.testapp.get('/user/api_keys', status=200)
+
+    def test_api_explorer(self):
+        self.testapp.get('/api-explorer', status=200)
+
+    def test_key_generation(self):
+        res = self.testapp.get('/user/api_keys', status=200)
+        tokens = self.session.query(BearerToken).filter_by(user_id=self.user.id).filter(BearerToken.revoked == False)
+        tokens_pre = len(tokens.all())
+        teststring = ''.join(random.choice(string.ascii_uppercase) for _ in range(10))
+        form = res.form
+        form["description"] = teststring
+        res_post_submit = form.submit("submit")
+        search_str = '/user/remove_api_key/'
+        start = res_post_submit.text.find(search_str)
+        start += len(search_str)
+        # Assumes token_id's < 1000
+        token_id = int(res_post_submit.text[start: start+3])
+        self.assertTrue(res.status.startswith('200'))
+        created_token = self.session.query(BearerToken).filter_by(user_id=self.user.id).filter(BearerToken.revoked == False).first()
+        tokens_post = len(tokens.all())
+        self.assertResContains(res_post_submit, teststring)
+        self.assertTrue(created_token.description == teststring)
+        self.assertTrue(created_token.id == token_id)
+        self.assertTrue((tokens_pre+1) == tokens_post)
