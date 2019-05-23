573be1f1a75a287e5c4813e19cd023d5332621bb
Tobias Wackenhut <tobias@wackenhut.at>
2019-05-23 22:56:11 +0200

    make Dockerfile production by default

--- docker-serve.sh
+++ docker-serve.sh
@@ -1,10 +1,21 @@
 #!/usr/bin/env bash
-sed 's/\/\/\//\/\/postgres@postgres\//' muesli.yml.example | sed 's/localhost/0.0.0.0/' > muesli.yml
-sed 's/\/\/\//\/\/postgres@postgres\//' alembic.ini.example > alembic.ini
-echo "Sleeping for 3s ..."; sleep 3;
-echo "Generating configs ... "
+if [[ -z "$MUESLI_TESTMODE" ]]
+then
+    sed 's/\/\/\//\/\/postgres@postgres\//' muesli.yml.example | sed 's/localhost/0.0.0.0/' > muesli.yml
+    sed 's/\/\/\//\/\/postgres@postgres\//' alembic.ini.example > alembic.ini
+    echo "Sleeping for 3s ..."; sleep 3;
+    echo "Generating configs ... "
+    python3 -m smtpd -n -c DebuggingServer localhost:25 &
+fi
+
+echo "Running database upgrade ... "
 alembic upgrade head
-python3 -m smtpd -n -c DebuggingServer localhost:25 &
 echo -n "IP-address: "
 ip -4 addr show | grep -oP "(?<=inet ).*(?=/)" | grep -ve "127.0.0.1"
-su -c /opt/muesli4/muesli-test muesli
+
+if [[ -z "$MUESLI_TESTMODE" ]]
+then
+    su -c /opt/muesli4/muesli-test muesli
+else
+    su -c /opt/muesli4/muesli-serve muesli
+fi
