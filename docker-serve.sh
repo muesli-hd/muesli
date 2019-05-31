e5a6d6a2e09453945884a09325adf4dfd6c8b912
Tobias Wackenhut <tobias@wackenhut.at>
2019-05-31 22:13:12 +0200

    enable reloading in testmode for all directories

--- docker-serve.sh
+++ docker-serve.sh
@@ -16,8 +16,11 @@ ip -4 addr show | grep -oP "(?<=inet ).*(?=/)" | grep -ve "127.0.0.1"
 
 if [[ -v MUESLI_TESTMODE ]]
 then
-    uwsgi --http :8080 --wsgi-file muesli.wsgi --callable application --fs-reload /opt/muesli4 --uid muesli \
-     --stats :8081 --disable-logging
+    # concatinate all filenames with --fs-reload
+    FSRELOADLIST=$(find /opt/muesli4 -type d -not -path '*/.git*' -not -path '*/__pycache__*' -print \
+     | sed -e ':a' -e 'N' -e '$!ba' -e 's/\n/ --fs-reload /g')
+    uwsgi --http :8080 --wsgi-file muesli.wsgi --callable application --uid muesli --disable-logging \
+    --fs-reload ${FSRELOADLIST}
 else
     uwsgi --socket :8080 --wsgi-file muesli.wsgi --callable application --master --processes 4 \
     --threaded-logger --uid muesli --stats :8081 --disable-logging --lazy-apps
