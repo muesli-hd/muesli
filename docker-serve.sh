4846d1a88bd8e191839fe7755385771a59e68184
Tobias Wackenhut <tobias@wackenhut.at>
2022-01-03 12:37:04 +0100

    Fix docker-serve development bug where reload did not work

--- docker-serve.sh
+++ docker-serve.sh
@@ -24,7 +24,7 @@ then
     FSRELOADLIST=$(find /opt/muesli4 -type d -not -path '*/.git*' -not -path '*/__pycache__*' -print \
      | sed -e ':a' -e 'N' -e '$!ba' -e 's/\n/ --fs-reload /g')
     uwsgi --http :8080 --wsgi-file muesli.wsgi --callable application \
-          --uid muesli --disable-logging --fs-reload "${FSRELOADLIST}"
+          --uid muesli --disable-logging --fs-reload ${FSRELOADLIST}
 else
     uwsgi --socket :8080 --wsgi-file muesli.wsgi --callable application \
           --master --processes 4 --threaded-logger --uid muesli --stats :8081 \
