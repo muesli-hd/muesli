8f5df791ee47b89a3274039f0bfd6dc8ea495cc6
Tobias Wackenhut <tobias@wackenhut.at>
2022-01-15 21:01:37 +0100

    Next attempt on a github actions fix

--- docker/run-tests.sh
+++ docker/run-tests.sh
@@ -5,7 +5,7 @@ SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 DOCKER_COMPOSE_PROJECT_OPTS=("-p" "muesli-tests" "--project-directory" "${SCRIPT_DIR}/../" "-f" \
  "${SCRIPT_DIR}/../docker-compose.yml" "-f" "${SCRIPT_DIR}/docker-compose.override.yml")
 
-if [[ $# -eq 2 ]] && [[ $1 -eq "--github-actions-image" ]]; then
+if [[ $# -eq 2 ]] && [[ $1 == "--github-actions-image" ]]; then
   cat > docker/docker-compose.github-actions.yml <<-EOFILE
 ---
 version: '3.2'
@@ -18,7 +18,9 @@ EOFILE
 
   DOCKER_COMPOSE_PROJECT_OPTS+=( "-f" "${SCRIPT_DIR}/docker-compose.github-actions.yml" )
 
-  docker-compose "${DOCKER_COMPOSE_PROJECT_OPTS[@]}" up --abort-on-container-exit --exit-code-from muesli --no-build "$@"
+  # Remove the first two script arguments
+  set -- "${@:3}"
+  echo docker-compose "${DOCKER_COMPOSE_PROJECT_OPTS[@]}" up --abort-on-container-exit --exit-code-from muesli --no-build "$@"
 else
   docker-compose "${DOCKER_COMPOSE_PROJECT_OPTS[@]}" up --abort-on-container-exit --exit-code-from muesli --build "$@"
   docker-compose "${DOCKER_COMPOSE_PROJECT_OPTS[@]}" down
