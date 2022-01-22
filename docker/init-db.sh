b7ca6d7d5e88aa19f74d02c059921f650c3e23b9
Tobias Wackenhut <tobias@wackenhut.at>
2022-01-22 21:55:47 +0100

    Harden database permissions

--- docker/init-db.sh
+++ docker/init-db.sh
@@ -4,19 +4,33 @@ set -e
 # This is necessary for both tests and loading production database dumps. The tests run on an empty database with suffix
 # test. To be as close to production as possible, we simply create a similar database layout here.
 
-psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
-    CREATE USER postgres PASSWORD 'muesli' SUPERUSER;
-    CREATE ROLE "muesli-admin";
-    GRANT "muesli-admin" TO "$POSTGRES_USER";
+psql -v ON_ERROR_STOP=1 --username "postgres" --dbname "postgres" <<-EOSQL
+  -- The DBMS has three users:
+  -- - postgres: Superuser, this script runs as
+  -- - muesli-admin: Owner of all tables and the muesli database. Alembic migrations use this
+  -- - muesli: Application user with minimal permissions
 
-    CREATE DATABASE mueslitest OWNER muesli;
+  CREATE USER "muesli-admin" PASSWORD 'mueslipw';
+  CREATE USER muesli PASSWORD 'mueslipw';
+
+  CREATE DATABASE mueslitest OWNER "muesli-admin";
+  CREATE DATABASE muesli OWNER "muesli-admin";
+
+  GRANT CONNECT ON DATABASE muesli TO muesli;
+EOSQL
+psql -v ON_ERROR_STOP=1 --username "postgres" --dbname "muesli" <<-EOSQL
+  GRANT USAGE ON SCHEMA public TO muesli;
+  GRANT SELECT, INSERT, UPDATE, DELETE ON ALL TABLES IN SCHEMA public TO muesli;
+  GRANT USAGE ON ALL SEQUENCES IN SCHEMA public TO muesli;
+  ALTER DEFAULT PRIVILEGES FOR USER "muesli-admin" IN SCHEMA public GRANT SELECT, INSERT, UPDATE, DELETE ON TABLES TO muesli;
+  ALTER DEFAULT PRIVILEGES FOR USER "muesli-admin" IN SCHEMA public GRANT USAGE ON SEQUENCES TO muesli;
 EOSQL
 
 # This allows restoring sql dumps in the compressed postgres format too
 if [[ -f "/muesli.prod.sql" ]]; then
   echo "Loading production database dump"
-  pg_restore -U "$POSTGRES_USER" -d "$POSTGRES_DB" -d muesli -n public -1 /muesli.prod.sql
+  pg_restore -U "muesli-admin" -d "muesli" -d muesli -n public -1 /muesli.prod.sql
 else
   echo "Loading development database dump"
-  psql -U "$POSTGRES_USER" -f /muesli.sql muesli
+  psql -U "muesli-admin" -f /muesli.sql muesli
 fi
