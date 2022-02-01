69c9ca6c33567acebaca9b854d609f4e6836cccf
Tobias Wackenhut <tobias@wackenhut.at>
2022-02-01 20:04:56 +0100

    Remove unused files sql.py, loadDatabase.py and exceptions.py

--- muesli/loadDatabase.py
+++
@@ -1,9 +0,0 @@
-# This file is for manual importing in python command line sessions. It is not used elsewhere in the codebase
-
-import sqlalchemy
-from . import models
-
-engine = sqlalchemy.create_engine('postgresql:///muesli', echo=False)
-Session = sqlalchemy.orm.sessionmaker(bind=engine)
-session = Session()
-#print(session.query(models.User).all())
