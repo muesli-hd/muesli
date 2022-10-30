28c698377db39c5659f87ec8dfaa1988616b66d4
Tobias Wackenhut <tobias@wackenhut.at>
2022-10-30 23:23:53 +0100

    Fix broken alembic migration.

--- alembic/versions/14d5e28d3626_lecture_assistants.py
+++ alembic/versions/14d5e28d3626_lecture_assistants.py
@@ -10,46 +10,49 @@ Create Date: 2012-04-13 11:50:50.501793
 revision = '14d5e28d3626'
 down_revision = '2a1638d875ac'
 
-from alembic import op
 import sqlalchemy as sa
+
+from alembic import op
+
 
 def upgrade():
     ### Caution: Only works in online mode!!!
     op.create_table('lecture_assistants',
-            sa.Column('lecture', sa.Integer(), nullable=False),
-            sa.Column('assistant', sa.Integer(), nullable=False),
-            sa.ForeignKeyConstraint(['assistant'], ['users.id'], ),
-            sa.ForeignKeyConstraint(['lecture'], ['lectures.id'], ),
-            sa.PrimaryKeyConstraint('lecture', 'assistant')
-    )
+                    sa.Column('lecture', sa.Integer(), nullable=False),
+                    sa.Column('assistant', sa.Integer(), nullable=False),
+                    sa.ForeignKeyConstraint(['assistant'], ['users.id'], ),
+                    sa.ForeignKeyConstraint(['lecture'], ['lectures.id'], ),
+                    sa.PrimaryKeyConstraint('lecture', 'assistant')
+                    )
     lectures = sa.sql.table('lectures',
-            sa.Column('id', sa.Integer),
-            sa.Column('assistant', sa.Integer)
-    )
+                            sa.Column('id', sa.Integer),
+                            sa.Column('assistant', sa.Integer)
+                            )
     connection = op.get_bind()
     results = connection.execute(lectures.select())
     ins = []
     for res in results:
-        if res.assistant!=None:
+        if res.assistant != None:
             ins.append({'lecture': res.id, 'assistant': res.assistant})
     lecture_assistants = sa.sql.table('lecture_assistants',
-            sa.Column('lecture', sa.Integer(), nullable=False),
-            sa.Column('assistant', sa.Integer(), nullable=False)
-    )
+                                      sa.Column('lecture', sa.Integer(), nullable=False),
+                                      sa.Column('assistant', sa.Integer(), nullable=False)
+                                      )
     if ins:
-        op.bulk_insert(lecture_assistants,ins)
+        op.bulk_insert(lecture_assistants, ins)
+
 
 def downgrade():
     lecture_assistants = sa.sql.table('lecture_assistants',
-            sa.Column('lecture', sa.Integer(), nullable=False),
-            sa.Column('assistant', sa.Integer(), nullable=False)
-    )
+                                      sa.Column('lecture', sa.Integer(), nullable=False),
+                                      sa.Column('assistant', sa.Integer(), nullable=False)
+                                      )
     lectures = sa.sql.table('lectures',
-            sa.Column('id', sa.Integer),
-            sa.Column('assistant', sa.Integer)
-    )
+                            sa.Column('id', sa.Integer),
+                            sa.Column('assistant', sa.Integer)
+                            )
     connection = op.get_bind()
     results = connection.execute(lecture_assistants.select())
     for res in results:
-        connection.execute(lectures.update(,.where(lectures.c.id == res.lecture).values(assistant=res.assistant))
+        connection.execute(lectures.update().where(lectures.c.id == res.lecture).values(assistant=res.assistant))
     op.drop_table('lecture_assistants')
